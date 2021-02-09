71c9b9606e04a96bf2703be05ef2140e16aea085
AlexTunder <rakutin2005@mail.ru>
2021-02-09 11:17:42 +0600

    tasks

--- 1.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <iostream>
-using namespace std;
-int main(){
-    short a, b, c, d;
-    cin >> a >> b >> c >> d;
-    long long result = a * b * c * d;
-    std::cout << result << endl;
-}
