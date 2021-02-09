48658011aaf966f0b83fab5afe3caa4bc4372b71
AlexTunder <rakutin2005@mail.ru>
2021-02-09 10:54:57 +0600

    Solutions for olymp at 09.02

---
+++ 1.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+int main(){
+    short a, b, c, d;
+    cin >> a >> b >> c >> d;
+    long long result = a * b * c * d;
+    std::cout << result << endl;
+}
---
+++ 3.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+int main(){
+    int n = 0;
+    int *bestArr = 0x0;
+    long long result = 0, ns = 0;
+    cin >> n;
+    bestArr = new int[n];
+    for(int i = 0; i < n; i++){
+        int a, b, c;
+        cin >> a >> b >> c;
+        if(a <= b && a <= c)
+            bestArr[i] = a;
+        else if(b <= c)
+            bestArr[i] = b;
+        else bestArr[i] = c;
+        if(result + bestArr[i] > 500) break;
+        else result += bestArr[i];
+        if(result <= 500) ns++;
+    }
+    printf("%i %i", ns, result);
+    // std::cout << result << endl;
+}
